12866b31273bb9db886d692c8e86eaa3eaea5180
github username <github email address>
2017-11-16 15:50:51 -0500

    minor changes

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -43,7 +43,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please send me a direct e-mail from your email client or click on the envelop icon at the bottom. ");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
